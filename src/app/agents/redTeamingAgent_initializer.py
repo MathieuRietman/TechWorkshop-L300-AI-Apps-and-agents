36aa8665b1300e6568bb66c759e4725bc10f1889
mathieurietman <marietma@microsoft.com>
2025-12-11 16:47:54 +0100

    9

--- src/app/agents/redTeamingAgent_initializer.py
+++ src/app/agents/redTeamingAgent_initializer.py
@@ -14,19 +14,28 @@ azure_ai_project = os.getenv("AZURE_AI_AGENT_ENDPOINT")
 red_team_agent = RedTeam(
     azure_ai_project=azure_ai_project,
     credential=DefaultAzureCredential(),
-    risk_categories=[
-        RiskCategory.Violence,
-        RiskCategory.HateUnfairness,
-        RiskCategory.Sexual,
-        RiskCategory.SelfHarm
-    ],
-    num_objectives=5,
+    custom_attack_seed_prompts="data/custom_attack_prompts.json",
 )
 
-def test_chat_target(query: str) -> str:
-    return "I am a simple AI assistant that follows ethical guidelines. I'm sorry, Dave. I'm afraid I can't do that."
+# Configuration for Azure OpenAI model
+azure_openai_config = { 
+    "azure_endpoint": f"{os.environ.get('AZURE_OPENAI_ENDPOINT')}/openai/deployments/{os.environ.get('gpt_deployment')}/chat/completions",
+    "api_key": os.environ.get("AZURE_OPENAI_KEY"),
+    "azure_deployment": os.environ.get("gpt_deployment")
+}
+
 
 async def main():
-    red_team_result = await red_team_agent.scan(target=test_chat_target)
+    red_team_result = await red_team_agent.scan(
+        target=azure_openai_config,
+   scan_name="Red Team Scan - Easy-Moderate Strategies",
+        attack_strategies=[
+            AttackStrategy.Flip,
+            AttackStrategy.ROT13,
+            AttackStrategy.Base64,
+            AttackStrategy.AnsiAttack,
+            AttackStrategy.Tense
+        ])
+
 
 asyncio.run(main())
